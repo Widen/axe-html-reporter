005cf2c59f18f4bfe88d650b92ad337482e92c67
Mark Skelton <mskelton@widen.com>
2022-02-11 15:05:19 -0600

    Fix

--- src/index.ts
+++ src/index.ts
@@ -32,47 +32,41 @@ export function createHtmlReport({ results, options }: CreateReport): string {
             "'violations' is required for HTML accessibility report. Example: createHtmlReport({ results : { violations: Result[] } })"
         );
     }
-    try {
-        const template = loadTemplate();
-        const preparedReportData = prepareReportData({
-            violations: results.violations,
-            passes: results.passes,
-            incomplete: results.incomplete,
-            inapplicable: results.inapplicable,
+    const template = loadTemplate();
+    const preparedReportData = prepareReportData({
+        violations: results.violations,
+        passes: results.passes,
+        incomplete: results.incomplete,
+        inapplicable: results.inapplicable,
+    });
+    const htmlContent = mustache.render(template, {
+        url: results.url,
+        violationsSummary: preparedReportData.violationsSummary,
+        violations: preparedReportData.violationsSummaryTable,
+        violationDetails: preparedReportData.violationsDetails,
+        checksPassed: preparedReportData.checksPassed,
+        checksIncomplete: preparedReportData.checksIncomplete,
+        checksInapplicable: preparedReportData.checksInapplicable,
+        hasPassed: Boolean(results.passes),
+        hasIncomplete: Boolean(results.incomplete),
+        hasInapplicable: Boolean(results.inapplicable),
+        incompleteTotal: preparedReportData.checksIncomplete
+            ? preparedReportData.checksIncomplete.length
+            : 0,
+        projectKey: options?.projectKey,
+        customSummary: options?.customSummary,
+        hasAxeRawResults: Boolean(results?.timestamp),
+        rules: prepareAxeRules(results?.toolOptions?.rules || {}),
+    });
+
+    if (!options?.doNotCreateReportFile) {
+        saveHtmlReport({
+            htmlContent,
+            reportFileName: options?.reportFileName,
+            outputDir: options?.outputDir,
+            outputDirPath: options?.outputDirPath,
         });
-        const htmlContent = mustache.render(template, {
-            url: results.url,
-            violationsSummary: preparedReportData.violationsSummary,
-            violations: preparedReportData.violationsSummaryTable,
-            violationDetails: preparedReportData.violationsDetails,
-            checksPassed: preparedReportData.checksPassed,
-            checksIncomplete: preparedReportData.checksIncomplete,
-            checksInapplicable: preparedReportData.checksInapplicable,
-            hasPassed: Boolean(results.passes),
-            hasIncomplete: Boolean(results.incomplete),
-            hasInapplicable: Boolean(results.inapplicable),
-            incompleteTotal: preparedReportData.checksIncomplete
-                ? preparedReportData.checksIncomplete.length
-                : 0,
-            projectKey: options?.projectKey,
-            customSummary: options?.customSummary,
-            hasAxeRawResults: Boolean(results?.timestamp),
-            rules: prepareAxeRules(results?.toolOptions?.rules || {}),
-        });
-
-        if (!options?.doNotCreateReportFile) {
-            saveHtmlReport({
-                htmlContent,
-                reportFileName: options?.reportFileName,
-                outputDir: options?.outputDir,
-                outputDirPath: options?.outputDirPath
-            });
-        }
-
-        return htmlContent;
-    } catch (e) {
-        console.warn(`HTML report was not created due to the error ${e.message}`);
-
-        return `Failed to create HTML report due to an error ${e.message}`;
     }
+
+    return htmlContent;
 }
